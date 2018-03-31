a76fc025036cf0ed7e5a60aca4bc92836394f9c0
truevoyage <35497138+truevoyage@users.noreply.github.com>
2018-03-30 21:07:52 -0400

    Attempted to fix Camera and Player animations/Scripts
    Added and edited some of the code for the player, guard, and camera to try and get them back to how they were during the demo.

--- ITCS 4231 Game/Assets/Scripts/PlayerManager.cs
+++ ITCS 4231 Game/Assets/Scripts/PlayerManager.cs
@@ -7,10 +7,10 @@ public class PlayerManager : MonoBehaviour {
     [SerializeField] private Animator anim;
     [SerializeField] private float lookSensitivity;
     [SerializeField] private Transform cam;
-    //private AudioClip footstep;
-    //AudioSource audioSource;
-    //[SerializeField] private float turnSpeed = 5.0f;
-    //[SerializeField] private Transform camTarget;
+    private AudioClip footstep;
+    AudioSource audioSource;
+    [SerializeField] private float turnSpeed = 5.0f;
+    [SerializeField] private Transform camTarget;
 
     // Use this for initialization
     void Start() {
@@ -87,14 +87,14 @@ public class PlayerManager : MonoBehaviour {
 
     public void SetDirection()
     {
-        //Vector3 towards = new Vector3(0f, cam.rotation.y, 0f).normalized;
-        //towards = Quaternion.Euler(towards.x, towards.y, towards.z);
-        //Debug.DrawRay(transform.position + Vector3.up, towards, Color.white);
-        //if (transform.rotation.y != cam.rotation.y)
-        //    transform.Rotate(towards); // Quaternion.Euler(towards.x, towards.y, towards.z);
-        //transform.rotation = Quaternion.LookRotation(towards, Vector3.up);
+        Vector3 towards = new Vector3(0f, cam.rotation.y, 0f).normalized;
+       // towards = Quaternion.Euler(towards.x, towards.y, towards.z);
+        Debug.DrawRay(transform.position + Vector3.up, towards, Color.white);
+        if (transform.rotation.y != cam.rotation.y)
+            transform.Rotate(towards); // Quaternion.Euler(towards.x, towards.y, towards.z);
+        transform.rotation = Quaternion.LookRotation(towards, Vector3.up);
 
-        Vector3 towards = transform.eulerAngles;
+        //Vector3 towards = transform.eulerAngles;
         towards.y = Camera.main.transform.eulerAngles.y;
         transform.eulerAngles = towards;
     }
