52e05c0498c69203a96250d5db31a13b6dab3370
Paul-PC\Paul <patkins2@uncc.edu>
2018-04-13 20:33:49 -0400

    Revert "Merge branch 'master' of https://github.com/patkins2/PrisonGame"
    This reverts commit a53176bc6cbecc173491d6af8f9a779504c08dbf, reversing
    changes made to 2469f6d3ec7e5fdb2143a86fc341e6fc52fd72b0.

---
+++ ITCS 4231 Game/Assets/Scripts/HashIDs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class HashIDs : MonoBehaviour {
+ 
+    public static HashIDs self;
+ 
+    public int playerMovementTypeInt;
+    public int guardMovementTypeInt;
+    public int playerStateInt;
+ 
+    void Awake () {
+ 
+        // The singleton reference hasn't been set yet -> set it
+        if (self == null) {
+            self = this;
+            DontDestroyOnLoad (gameObject);
+        } 
+        // The singleton already exists -> destroy this one
+        else {
+             
+            Destroy (gameObject);
+        }
+
+        playerMovementTypeInt = Animator.StringToHash("playerMovementType");
+        playerStateInt = Animator.StringToHash("playerState");
+
+        guardMovementTypeInt = Animator.StringToHash("guardMovementType");
+    }
+ 
+}
