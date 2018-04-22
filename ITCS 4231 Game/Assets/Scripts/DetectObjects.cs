cf6ef35394de88d40d1e685a83f31a1ba76591b6
Casey <cbrow267@uncc.edu>
2018-04-22 00:36:51 -0400

    more player features
    -movement in all four directions when standing
    - can open doors

---
+++ ITCS 4231 Game/Assets/Scripts/DetectObjects.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObjects : MonoBehaviour
+{
+
+    [SerializeField] private float openDist = 3;
+    private float dist = 0;
+    [SerializeField] private Animator doorAnim;
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform door;
+    public GameObject detectedObject;
+    public string detected = "";
+
+
+    void Update()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, (transform.position - Camera.main.transform.position), Color.red);
+
+        if (Physics.Raycast(transform.position, (transform.position - Camera.main.transform.position), out hit))
+        {
+            if (hit.collider.tag == "Door" && hit.distance < openDist)
+            {
+                dist = hit.distance;
+                detected = "door";
+                detectedObject = hit.collider.gameObject;
+            }
+            else
+            {
+                dist = 0f;
+                detected = "";
+            }
+            //TODO check for item detected
+        }
+    }
+}
--- ITCS 4231 Game/Assets/Scripts/PlayerManager.cs
+++ ITCS 4231 Game/Assets/Scripts/PlayerManager.cs
@@ -7,14 +7,11 @@ public class PlayerManager : MonoBehaviour {
     [SerializeField] private Animator anim;
     [SerializeField] private float lookSensitivity;
     [SerializeField] private Transform cam;
-    //private AudioClip footstep;
-    //AudioSource audioSource;
+    [SerializeField] private GameObject camTarget;
     //[SerializeField] private float turnSpeed = 5.0f;
-    //[SerializeField] private Transform camTarget;
 
     // Use this for initialization
     void Start() {
-        //audioSource = GetComponent<AudioSource>();
         anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.idle);
         anim.SetInteger(HashIDs.self.playerStateInt, (int)PlayerState.standing);
     }
@@ -22,57 +19,52 @@ public class PlayerManager : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        //print("Movement type = " + HashIDs.self.playerMovementTypeInt);
-        // TODO set forward running direction equal to direction camera is facing
-
         float xmove = Input.GetAxis("Horizontal");
         float zmove = Input.GetAxis("Vertical");
 
-        //print("xmove = " + xmove);
-        //print("zmove = " + zmove);
-        //print("state " + HashIDs.self.playerMovementTypeInt);
-        
+        //print("player" + transform.position);
+        //Player is not moving
+        if (xmove == 0 && zmove == 0) { anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.idle); }
+        //Press W to move forward
+        if (zmove > 0) { anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.forward); }
+        //Press S to move backward
+        if (zmove < 0) { anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.backward); }
+        //Press D to move right
+        if (xmove > 0) { anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.right); }
+        //Press A to move left
+        if (xmove < 0) { anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.left); }
+        //Press spacebar to roll forward
 
-        if(xmove == 0 && zmove == 0)        //Player is not moving
+        if (Input.GetKeyDown(KeyCode.Space)) { anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.dive); }
+        //Press C to toggle crouch
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            // Set moveType to 0 to stay in idle animation
-            anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.idle);
-            //print("Movement: " + playerMovementTypeInt.idle);
-        }
-        if (zmove > 0)      //Moving forward
-        {
-            anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.forward);
-        }
-        else if(zmove < 0)       //Moving backwards
-        {
-            anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.backward);
-        }
-        else if (xmove > 0)
-        {
-            anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.right);
-        }
-        else if (xmove < 0)
-        {
-            anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.left);
-        }
-        else if (Input.GetKeyDown(KeyCode.C))
-        {
-            print("State int : " + anim.GetInteger(HashIDs.self.playerStateInt));
             if (anim.GetInteger(HashIDs.self.playerStateInt) == 0) {
-                print("time to crouch!");
                 anim.SetInteger(HashIDs.self.playerStateInt, (int)PlayerState.crouched);
             }
             else if (anim.GetInteger(HashIDs.self.playerStateInt) == 1) {
-                print("Standing up!");
                 anim.SetInteger(HashIDs.self.playerStateInt, (int)PlayerState.standing);
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            anim.SetInteger(HashIDs.self.playerMovementTypeInt, (int)PlayerMovementType.dive);
+        //Press tab to switch camera shoulder
+        if(Input.GetKeyDown(KeyCode.Tab))
+        { 
+            if (camTarget.transform.localPosition.x > 0f)
+                camTarget.transform.Translate(-(0.25f * 2), 0f, 0f);
+            else if (camTarget.transform.localPosition.x < 0f)
+                camTarget.transform.Translate((0.25f * 2), 0f, 0f);
         }
 
+        //TODO check which object is detected (door or item) and call appropriate function
+        //Press F to interact with door
+        if (Input.GetKeyDown(KeyCode.F) && camTarget.GetComponent<DetectObjects>().detected == "door")
+        {
+            
+            GameObject door = camTarget.GetComponent<DetectObjects>().detectedObject;
+            door.GetComponent<DoorController>().OpenClose();
+            print("ITS DOOR TIME");
+        }
     }
 
    /* void playFootStep()
@@ -88,13 +80,6 @@ public class PlayerManager : MonoBehaviour {
 
     public void SetDirection()
     {
-        //Vector3 towards = new Vector3(0f, cam.rotation.y, 0f).normalized;
-        //towards = Quaternion.Euler(towards.x, towards.y, towards.z);
-        //Debug.DrawRay(transform.position + Vector3.up, towards, Color.white);
-        //if (transform.rotation.y != cam.rotation.y)
-        //    transform.Rotate(towards); // Quaternion.Euler(towards.x, towards.y, towards.z);
-        //transform.rotation = Quaternion.LookRotation(towards, Vector3.up);
-
         Vector3 towards = transform.eulerAngles;
         towards.y = Camera.main.transform.eulerAngles.y;
         transform.eulerAngles = towards;
